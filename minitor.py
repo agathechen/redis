ef4478464f7346c0f7aa829d9fc3de672903d498
agathechen <2754197835@qq.com>
2022-07-26 12:06:24 +0800

    minitor

--- minitor.py
+++ minitor.py
@@ -6,8 +6,7 @@ redis = StrictRedis(host='localhost', port=6379)
 pubsub = redis.pubsub()
 
 def event_handler(msg):
-    def event_handler(msg):
-        print('Handler', msg)
+    print('Handler', msg)
     #handler=msg['data']
     #本来是想通过返回的msg信息直接定位修改的id和price的，但是发现返回的msg里并不包含value里的值
     # if str(handler)=='hset':
